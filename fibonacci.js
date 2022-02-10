6dd5fc28eca3f54fb3f566ce0872d8a684fcbc99
josergavila <jose.avila@vorp.com>
2022-02-10 09:12:34 -0300

    added fibonacci table implementation

--- fibonacci.js
+++ fibonacci.js
@@ -7,7 +7,7 @@
 const fib1 = (n) => {
 	if (n <= 2) return 1;
 	return fib1(n - 1) + fib1(n - 2);
-}
+};
 console.log(fib1(6));
 
 
@@ -19,6 +19,18 @@ const fib2 = (n, memo = {}) => {
 	if (n <= 2) return 1;
 	memo[n] = fib2(n - 1, memo) + fib2(n - 2, memo);
 	return memo[n];
-}
+};
 console.log(fib2(6));
 console.log(fib2(50));
+
+// (iii) fibonacci table (linear complexity)
+ const fib = (n) => {
+	 const table = Array(n + 1).fill(0);
+	 table[1] = 1;
+	 for (let i = 0; i <= n; i++) {
+		 table[i + 1] += table[i];
+		 table[i + 2] += table[i];
+	}
+	return table[n];
+ };
+console.log(fib(6));

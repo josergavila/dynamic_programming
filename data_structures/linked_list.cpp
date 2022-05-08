03c043e88e5c3ee5cca74db1be8a668b9a46380f
josergavila <jose.avila@vorp.com>
2022-05-08 10:47:30 -0300

    improved linked list script

--- data_structures/linked_list.cpp
+++ data_structures/linked_list.cpp
@@ -5,7 +5,8 @@ struct Node
     int data;
     struct Node* next;
 };
-struct Node* head = NULL;
+struct Node* head;
+int size = 0;
 
 void insert(int new_data)
 {
@@ -15,10 +16,36 @@ void insert(int new_data)
     head = new_node;
 }
 
-void display()
+void insertPos(Node** current, int pos, int data)
+{
+	if (pos < 1 || pos > size + 1) 
+	{
+		std::cout << "Invalid Position!\n" << std::endl;
+	}
+	else
+	{
+		while (pos--) {
+			if (pos == 0)
+			{
+				Node* temp = getNode(data);
+				temp->next = *current;
+				*current = temp;
+			}
+			else
+			{
+
+
+			}
+		}
+		size++;
+	}
+}
+
+void Print()
 {
     struct Node* ptr;
     ptr = head;
+    std::cout << "Linked list is: ";
     while(ptr != NULL)
     {
         std::cout << ptr->data << " ";
@@ -29,11 +56,19 @@ void display()
 
 int main()
 {
-    insert(10);
-    insert(5);
-    insert(8);
-    insert(9);
-    std::cout << "Linked list is: ";
-    display();
+	head = NULL;
+	std::cout << "How many numbers?" << std::endl;
+	int n, i, x;
+	std::cin >> n;
+	for (i = 0; i < n; i++) 
+	{
+		std::cout << "Enter number: ";
+		std::cin >> x;
+		insert(x);
+		std::cout << "\nNumber Added";
+	}
+	size = n;
+
+    Print();
     return 0;
 }
